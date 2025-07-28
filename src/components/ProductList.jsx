2c88c7d9bdfb4c9e508a808f28c4dc292691066e
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-28 13:19:09 +0200

    refactor ProductList to make the axios request, refactor ProductPage.jsx to be a wrapper page

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,6 +1,21 @@
 import ProductCard from "./ProductCard";
+import { useProducts } from "../contexts/ProductsContext";
+import axios from "axios";
+import { useEffect } from "react";
 
-export default function ProductList({ products }) {
+export default function ProductList() {
+  const { products, setProducts } = useProducts();
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        setProducts(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
       <div className="row">
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,29 +1,14 @@
 import ProductList from "../components/ProductList";
 
-import { useEffect } from "react";
-import axios from "axios";
 //*import context
-import { useProducts } from "../contexts/ProductsContext";
 
-export default function ProductsPage() {
-  const { products, setProducts } = useProducts();
-
-  useEffect(() => {
-    axios
-      .get("https://fakestoreapi.com/products")
-      .then((res) => {
-        setProducts(res.data);
-        console.log(res.data);
-      })
-      .catch((err) => console.error(err));
-  }, []);
-
+export default function ProductsPage({ products }) {
   return (
     <main>
       <div className="container py-5">
         <h2 className="mb-4">I nostri prodotti</h2>
+        <ProductList products={products} />
       </div>
-      <ProductList products={products} />
     </main>
   );
 }
